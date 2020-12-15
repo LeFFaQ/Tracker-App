97a72ce78c96614e8701f1d1b7a241b07065a1fc
LeFFaQ <lffqenterprise@gmail.com>
2020-12-15 15:29:52 +0700

    added functionality for geolocation

--- app/src/main/java/com/lffq/trackerapp/MainActivity.kt
+++ app/src/main/java/com/lffq/trackerapp/MainActivity.kt
@@ -1,29 +1,95 @@
 package com.lffq.trackerapp
 
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.lffq.trackerapp.other.Constants
+import com.lffq.trackerapp.other.Constants.TAG
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
     private val appBarConfiguration by lazy { AppBarConfiguration(navController.graph) }
-
+    lateinit var fusedLocationClient: FusedLocationProviderClient
+    lateinit var locationRequest: LocationRequest
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(applicationContext)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.findNavController()
 
         setSupportActionBar(toolbar)
         setupActionBarWithNavController(navController)
+
+        RequestPermission()
+        getLastLocation()
+    }
+
+    @SuppressLint("MissingPermission")
+    fun getLastLocation(){
+        if(CheckPermission()){
+            if(isLocationEnabled()){
+                fusedLocationClient.lastLocation.addOnCompleteListener {task->
+                    val location: Location? = task.result
+                    if (location == null) {
+                        Log.d("Debug:" ,"Error")
+                    } else {
+                        Log.d("Debug:" ,"Your Location:"+ location.longitude)
+                    }
+                }
+            } else { Toast.makeText(this,"Please Turn on Your device Location",Toast.LENGTH_SHORT).show() }
+        } else{ RequestPermission() }
+    }
+
+    fun CheckPermission():Boolean{
+        //this function will return a boolean
+        //true: if we have permission
+        //false if not
+        if(ActivityCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION) == PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) == PERMISSION_GRANTED
+        ){
+            return true
+        }
+        return false
+    }
+
+    fun RequestPermission(){
+        //this function will allows us to tell the user to requesut the necessary permsiion if they are not garented
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION),
+            Constants.REQUEST_PERMISSIONS_REQUEST_CODE
+        )
+    }
+
+    fun isLocationEnabled():Boolean{
+        //this function will return to us the state of the location service
+        //if the gps or the network provider is enabled then it will return true otherwise it will return false
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -35,3 +101,4 @@ class MainActivity : AppCompatActivity() {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
+
--- app/src/main/java/com/lffq/trackerapp/other/Constants.kt
+++ app/src/main/java/com/lffq/trackerapp/other/Constants.kt
@@ -4,4 +4,7 @@ object Constants {
 
     const val TYPE_STATS = 0
     const val TYPE_CONTACTS = 1
+
+    const val TAG = "123"
+    const val REQUEST_PERMISSIONS_REQUEST_CODE = 34
 }
