ceb8b61d8436e73b4109345ad778585283ea9c49
LeFFaQ <lffqenterprise@gmail.com>
2020-12-17 16:23:20 +0700

    little improvements

--- app/src/main/java/com/lffq/trackerapp/MainActivity.kt
+++ app/src/main/java/com/lffq/trackerapp/MainActivity.kt
@@ -10,25 +10,23 @@ import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
 import com.lffq.trackerapp.other.Constants
-import com.lffq.trackerapp.other.Constants.TAG
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
-    // private val appBarConfiguration by lazy { AppBarConfiguration(navController.graph) }
     lateinit var fusedLocationClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,15 +51,14 @@ class MainActivity : AppCompatActivity() {
                 fusedLocationClient.lastLocation.addOnCompleteListener {task->
                     val location: Location? = task.result
                     if (location == null) {
-                        Log.d("Debug:" ,"Error")
+                        Log.d("Location:" ,"Error")
                     } else {
-                        Log.d("Debug:" ,"Your Location:"+ location.longitude)
+                        Log.d("Location:" ,"Your Location:"+ location.longitude)
                     }
                 }
             } else { Toast.makeText(this,"Please Turn on Your device Location",Toast.LENGTH_SHORT).show() }
         } else{ RequestPermission() }
     }
-
     fun CheckPermission():Boolean{
         //this function will return a boolean
         //true: if we have permission
@@ -73,7 +70,6 @@ class MainActivity : AppCompatActivity() {
         }
         return false
     }
-
     fun RequestPermission(){
         //this function will allows us to tell the user to requesut the necessary permsiion if they are not garented
         ActivityCompat.requestPermissions(
@@ -82,7 +78,6 @@ class MainActivity : AppCompatActivity() {
             Constants.REQUEST_PERMISSIONS_REQUEST_CODE
         )
     }
-
     fun isLocationEnabled():Boolean{
         //this function will return to us the state of the location service
         //if the gps or the network provider is enabled then it will return true otherwise it will return false
@@ -95,7 +90,16 @@ class MainActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.app_menu, menu)
         return true
     }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
+        return if (item.itemId == R.id.test_mode) {
+
+            val action = NavGraphDirections.actionGlobalTestModeFragment()
+            navController.navigate(action)
+            true
+        } else { return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item) }
+
+    }
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
